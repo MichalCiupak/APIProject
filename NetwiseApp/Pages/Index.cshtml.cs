f0af1122cac1e104827fdc97d1dca27615d55fd7
MichalCiupak <michalciupak.26@gmail.com>
2024-05-12 12:38:34 +0200

    Implemented functionality for saving cat data to file

--- NetwiseApp/Pages/Index.cshtml.cs
+++ NetwiseApp/Pages/Index.cshtml.cs
@@ -1,20 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetwiseApp.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace NetwiseApp.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel : PageModel
+	{
+		private readonly CatFactService _catFactService;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+		public IndexModel(CatFactService catFactService)
+		{
+			_catFactService = catFactService;
+		}
 
-        public void OnGet()
-        {
+		[BindProperty]
+		public string CatFactMessage { get; set; }
 
-        }
-    }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			string? catFact = await _catFactService.GetCatFact();
+			if (!string.IsNullOrEmpty(catFact))
+			{
+				string filePath = "cat_facts.txt";
+				string newLine = $"{DateTime.Now}: {catFact}";
+
+				using (StreamWriter writer = System.IO.File.AppendText(filePath))
+				{
+					await writer.WriteLineAsync(newLine);
+				}
+
+				CatFactMessage = catFact;
+			}
+
+			return Page();
+		}
+	}
 }
--- NetwiseApp/Program.cs
+++ NetwiseApp/Program.cs
@@ -1,7 +1,10 @@
+using NetwiseApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpClient<CatFactService>();
 
 var app = builder.Build();
 
