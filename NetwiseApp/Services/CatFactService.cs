c98e615dce362c96b7072ab9915b53cabcde4179
MichalCiupak <michalciupak.26@gmail.com>
2024-05-12 11:59:42 +0200

    Add a service that fetch facts about cats

---
+++ NetwiseApp/Services/CatFactService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NetwiseApp.Models;
+
+
+
+namespace NetwiseApp.Services
+{
+    public class CatFactService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatFactService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string?> GetCatFact()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://catfact.ninja/fact");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                var factObject = JsonSerializer.Deserialize<CatFactResponse>(responseData);
+                if (factObject != null && factObject.Fact != null)
+                {
+                    return factObject.Fact;
+                }
+            }
+            return null;
+        }
+    }
+}
