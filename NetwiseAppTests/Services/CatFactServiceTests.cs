99f3157d4498488fa2a31e60bb4086c78a0a4c0e
MichalCiupak <michalciupak.26@gmail.com>
2024-05-12 13:10:04 +0200

    Add a test for cat fact service data fetching

---
+++ NetwiseAppTests/Services/CatFactServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NetwiseApp.Services;
+using System.Net;
+using RichardSzalay.MockHttp;
+
+namespace NetwiseAppTests.Services
+{
+	public class CatFactServiceTests
+	{
+		[Fact]
+		public async Task CatFactServicesTestAsync()
+		{
+			var mockHttp = new MockHttpMessageHandler();
+			var responseContent = "{\"fact\": \"Test Cat Fact\", \"length\": 13}";
+			mockHttp.When("https://catfact.ninja/fact")
+					.Respond(HttpStatusCode.OK, "application/json", responseContent);
+
+			var httpClient = new HttpClient(mockHttp);
+			var catFactService = new CatFactService(httpClient);
+			var catFact = await catFactService.GetCatFact();
+			Assert.Equal("Test Cat Fact", catFact);
+		}
+	}
+}
